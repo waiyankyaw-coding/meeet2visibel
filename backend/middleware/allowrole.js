99d656e1961f571911c36e7bef9972655788287b
kiee <kieekieepl@gmail.com.com>
2026-07-19 19:14:13 +0700

    meet2visibel_fix_err_rdy

--- backend/middleware/allowrole.js
+++
@@ -1,9 +0,0 @@
-function requireRole(allowedRoles) {
-  return (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: 'Forbidden: insufficient permissions' });
-    }
-    next();
-  };
-};
-module.exports = requireRole;
